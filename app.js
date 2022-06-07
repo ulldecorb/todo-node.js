7304fb4eb5d09d1e1b120109cc12fc3d29ed2a5d
ulldecorb <ausias.bertran@gmail.com>
2022-06-07 02:20:43 +0200

    Add routes

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { use } = require('express/lib/application');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -14,7 +13,7 @@ mongoose.connect(process.env.DDBB_URL,
 )
 
 // middlewares
-app-use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use( express.static( 'public' ));
 app.set( 'view engine', 'ejs');
 
@@ -22,11 +21,15 @@ app.set( 'view engine', 'ejs');
 app.use(express.json());
 
 // routes
+app.use(require('./routes/index'));
 
-app.get('/', async ( req, res ) => {
-    console.log(await req.body)
+
+app.get('/',  ( req, res ) => {
+    console.log( req.body);
+    res.send('helo')
+
     // const products = await res.json(res.body.products);
     // res.send(products)
 })
 
-app.listen(8080, () => console.log('Server run on port 8080'));
+app.listen(port, () => console.log(`Server run on port ${port}`));
