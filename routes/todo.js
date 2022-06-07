f91cde29d3ed8023cf47396bae008069432d6867
ulldecorb <ausias.bertran@gmail.com>
2022-06-08 01:42:30 +0200

    Add delete task functionality

--- routes/todo.js
+++ routes/todo.js
@@ -6,10 +6,22 @@ router.post('/add/todo', async ( req, res ) => {
     const {todo} = req.body;
     const newTodo = new Todo({todo});
 
-    newTodo.save().then(() => {
-        console.log(`${chalk.blue('Todo')} added ${chalk.green('Successfully')}`);
+    newTodo.save()
+    .then(() => {
+        console.log(`${chalk.blue(`${todo}`)} added ${chalk.green('Successfully')}`);
         res.redirect('/');
     })
+    .catch((err) => console.log(chalk.red(`${err}`)));
 })
+
+.get('delete/todo/:_id', async ( req, res ) => {
+    const {_id } = req.params;
+    Todo.deleteOne({ _id })
+        .then(() => {
+            console.log(chalk.yellow('Deleted Todo Successfully'));
+            res.redirect('/');
+        })
+        .catch((err) => console.log(chalk.red(`${err}`)))
+});
 
 module.exports = router;
